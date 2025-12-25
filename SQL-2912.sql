c967752caf62e67828c5b28117b9deb931398892
quangtuan-0210 <tpck1234567890@gmail.com>
2025-12-25 22:55:54 +0700

    Create SQL-2912.sql

---
+++ SQL-2912.sql
@@ -0,0 +1,15 @@
+select year, sum(budget) as total_budget
+from (SELECT budget, year, country
+FROM ai_budget_usa
+UNION ALL
+SELECT budget, year, country
+FROM ai_budget_uk
+UNION ALL
+SELECT budget, year, country
+FROM ai_budget_japan
+) as tmp
+where year>=2018 and year<=2021 
+group by year
+order by total_budget desc
+limit 1
+
