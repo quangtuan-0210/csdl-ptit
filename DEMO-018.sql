74d05d0b889960395759557a78a3ef5503eca797
quangtuan-0210 <tpck1234567890@gmail.com>
2025-12-27 13:52:41 +0700

    Create DEMO-018.sql

---
+++ DEMO-018.sql
@@ -0,0 +1,8 @@
+alter table Employees 
+modify column emp_id int not null auto_increment,
+add primary key(emp_id),
+modify column salary decimal(12,2) not null default 0,
+modify column status enum('ACTIVE','INACTIVE','RESIGNED') not null default 'ACTIVE',
+add column employment_type enum('FULL_TIME', 'PART_TIME','INTERN') not null default 'FULL_TIME',
+add column hire_date date,
+drop column temp_info
